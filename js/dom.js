47da3f406cb4a5226282e3507eff68a13ca9e6cf
Alex Martins <alexmartins.profissional@gmail.com>
2024-04-10 11:47:51 -0300

    feat: add dom manipulation lesssons

--- js/dom.js
+++ js/dom.js
@@ -56,3 +56,44 @@ console.log("querySelector usando hierarquia - DOM Image: ");
 
 console.log(domImg);
 
+// Propriedades dos objetos 
+
+//Cada um dos objetos da dom tem propriedades e métodos diferentes que podem ser acessados e manipulados
+// através do ponto (.) após o objeto selecionado
+
+//textContent
+
+const cardI = document.querySelector('#card-1');
+
+cardI.textContent = "Texto alterado com textContent";
+
+//innerHTML
+
+const cardIII = document.querySelector('#card-3');
+
+cardIII.innerHTML = "<button class='border-2 border-black'> Botão criado com innerHTML </button>";
+
+//style
+
+const cardIV = document.querySelector('#card-4');
+
+cardIV.style.backgroundColor = "blue";
+
+//classList
+
+const cardII = document.querySelector('#card-2');
+cardII.classList.add('border-2', 'border-black');
+
+
+//getAttribute
+
+const startButton = document.querySelector('#start-button a');
+
+console.log("getAttribute - Start Button: ");
+console.log(startButton.getAttribute('href'));
+
+//setAttribute
+console.log("setAttribute - Start Button: ");
+startButton.setAttribute('href', 'https://www.google.com');
+
+
